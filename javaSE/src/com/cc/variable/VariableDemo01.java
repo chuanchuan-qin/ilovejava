d98604a8826d36a133738fb30e899149aa9aa90b
chuanchuan <2803022133@qq.com>
2026-07-11 03:29:54 +0800

    新增java基础语法

---
+++ javaSE/src/com/cc/variable/VariableDemo01.java
@@ -0,0 +1,13 @@
+package com.cc.variable;
+
+public class VariableDemo01 {
+    public static void main(String[] args){
+        int age;           // 声明
+        age = 18;          // 赋值
+        double pi = 3.14;  // 声明并初始化
+
+        //可以在一行声明多个同类型变量（不推荐）：
+        int a, b, c;
+        int x = 1, y = 2;
+    }
+}
